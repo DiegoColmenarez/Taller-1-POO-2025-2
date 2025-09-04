0c6a0bc84f57dd447be17ba0ec3f58ce4db97fba
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2025-09-04 15:16:59 -0500

    Actualizando la clase Libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,36 @@
 public class Libro {
+    String autor;
+    String titulo;
+    String numPaginas;
+    public Libro(){
+        autor = "Albert Camus";
+        titulo = "La peste";
+        numPaginas = "364";
+    }
+    private Libro(String autor1, String obraTitulo, String numeroPaginas){
+        autor = autor1;
+        titulo = obraTitulo;
+        numPaginas = numeroPaginas;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getNumPaginas() {
+        return numPaginas;
+    }
+    public static void main(String[] args) {
+        Libro obra = new Libro();
+        System.out.println("Titulo: " + obra.titulo);
+        System.out.println("Numero de paginas: " + obra.numPaginas);
+        System.out.println("Autor: " + obra.autor);
+        Libro obra2 = new Libro("Gabriel Garcia Marquez", "Cronicas de una muerte anunciada", "80");
+        System.out.println("Titulo: " + obra2.getTitulo());
+        System.out.println("Numero de paginas: " + obra2.getNumPaginas());
+        System.out.println("Autor: " + obra2.getAutor());
+    }
 }
+
+
